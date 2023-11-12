f90c17a160731c9822efcd6e1f77293c0b28a9af
Fospx <marcosgarcia2485@gmail.com>
2023-11-12 16:07:39 -0500

    added what the computer chose

--- rps.js
+++ rps.js
@@ -27,6 +27,7 @@ function playRound(playerSelection, computerSelection)
 
 const playerSelection = "rock";
 const computerSelection = getComputerChoice();
+console.log("Computer chose: " + computerSelection);
 console.log(playRound(playerSelection, computerSelection));
 
 
