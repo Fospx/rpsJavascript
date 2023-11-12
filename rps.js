278abcaea563db07d011177ddf8e79302997499f
Fospx <marcosgarcia2485@gmail.com>
2023-11-12 15:57:27 -0500

    created an array with rpsand used a built in function to get a random element in the array

--- rps.js
+++ rps.js
@@ -1,36 +1,75 @@
 // Javascript
 
+function getComputerChoice()
+{
+    rpsArray = ['rock', 'paper', 'scissor'];
+    compChoice = (rpsArray[(Math.floor(Math.random() * rpsArray.length))])
+    return compChoice;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // An arry with 
-function getComputerChoice(){
-    rpsArray = ['rock', 'paper', 'scissors'];
-    computerChoice = (rpsArray[(Math.floor(Math.random() * rpsArray.length))]);
-    return computerChoice;
-}
+// function getComputerChoice(){
+//     rpsArray = ['rock', 'paper', 'scissors'];
+//     computerChoice = (rpsArray[(Math.floor(Math.random() * rpsArray.length))]);
+//     return computerChoice;
+// }
 
-function playRound(playerSelection, computerSelection){
+// function playRound(playerSelection, computerSelection){
     
-    // DRAW CONDITIONS
-    if (playerSelection === computerSelection)
-    {
-        return "It's a tie";
-    }
-    // WIN CONDITIONS
-    else if ((playerSelection === 'rock' && computerSelection === 'scissors') || 
-    (playerSelection === 'paper' && computerSelection === 'rock') || 
-    (playerSelection === "scissors" && computerSelection === 'paper'))
-    {
-        return "you win";
-    }
-    else {
-        return "You Lose! " + computerSelection + " beats " + playerSelection;
-    } 
+//     // DRAW CONDITIONS
+//     if (playerSelection === computerSelection)
+//     {
+//         return "It's a tie";
+//     }
+//     // WIN CONDITIONS
+//     else if ((playerSelection === 'rock' && computerSelection === 'scissors') || 
+//     (playerSelection === 'paper' && computerSelection === 'rock') || 
+//     (playerSelection === "scissors" && computerSelection === 'paper'))
+//     {
+//         return "you win";
+//     }
+//     else {
+//         return "You Lose! " + computerSelection + " beats " + playerSelection;
+//     } 
  
-}
+// }
 
-const playerSelection = "paper"
-const computerSelection = getComputerChoice();
-console.log("computers choice: " + computerSelection);
-console.log(playRound(playerSelection, computerSelection));
+// const playerSelection = "paper"
+// const computerSelection = getComputerChoice();
+// console.log("computers choice: " + computerSelection);
+// console.log(playRound(playerSelection, computerSelection));
+
+
+
+
 
 
 
